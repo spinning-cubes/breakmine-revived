5d102cce95d256d89329e874c04cea53483f0828
spinning-cubes <spiin100@outlook.com>
2026-07-26 11:50:39 -0400

    marw-programmer

--- handlers.js
+++ handlers.js
@@ -192,6 +192,7 @@ function handlePlayPacket(player, packetId, buffer, offset) {
                 if (player.username === "developer") newname = '§4' + player.username + '§7';
                 if (player.username === "kai") newname = '§b' + player.username + '§7';
                 if (player.username === "LowQualityCoding") newname = '§a</>§f ' + player.username + '§7';
+                if (player.username === "Marw-Programmer") newname = '§a' + player.username + '§7';
                 sendChatMessage(`§7<§f${newname}§7> §f${msg}`);
             }
             break;
