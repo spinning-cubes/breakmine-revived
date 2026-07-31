e2a7a1a40566523fac6d909fa0dc2c9fddada918
spinning-cubes <spiin100@outlook.com>
2026-07-31 01:26:04 -0400

    patch Patchwork :p

--- example_mods/archive/ModData.js
+++
@@ -1,9 +0,0 @@
-export default class ModData {
-    static NAME = "Bombs And Shiz";
-
-    static ID = "bombsandshiz";
-
-    static AUTHOR = "LowQualityCoding";
-    
-    static VERSION = "1.0.0";
-}
